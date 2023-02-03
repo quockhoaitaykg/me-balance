167573f140ef25c6a64b6ee3c56967c34a6e91e7
Quốc Nguyễn <quockhoaitay99@gmail.com>
2023-02-03 21:43:59 +0700

    Add sign in page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import Authen from './pages/authentication/authen'
+import { Switch, Route, Redirect } from "react-router-dom";
 import { Layout } from "antd";
-import { BrowserRouter as Router, Routes, Route  } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import "./assets/styles/main.css";
+import "./assets/styles/responsive.css";
 import './App.css';
 
 function App() {
   const { Content } = Layout;
   return (
     <div className="App">
-      <Layout >
-          <Content style={{ padding: "10%", height: "100%", minHeight: "100vh", width: "100%" }}>
-              <Router>
-                <Routes>
-                  <Route path="/" element={<Authen />} />
-                </Routes>
-            </Router>
-          </Content>
-      </Layout>
+      <Switch>
+        {/* <Route path="/sign-up" exact component={SignUp} /> */}
+        <Route path="/sign-in" exact component={SignIn} />
+        <Redirect from="*" to="/sign-in" />
+        {/* <Main>
+          <Route exact path="/dashboard" component={Home} />
+          <Route exact path="/tables" component={Tables} />
+          <Route exact path="/billing" component={Billing} />
+          <Route exact path="/rtl" component={Rtl} />
+          <Route exact path="/profile" component={Profile} />
+          <Redirect from="*" to="/dashboard" />
+        </Main> */}
+      </Switch>
     </div>
   );
 }
